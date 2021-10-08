ea892a5aebb48fca7ee2387eb9a11ab2804e5904
Navid Mehralizadeh <n.mehralizadeh@khodro45.com>
2021-10-09 01:58:06 +0330

    Add Base Controller

---
+++ SquareFish.Assessment.API/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SquareFish.Assessment.API.Controllers
+{
+    public class BaseController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
